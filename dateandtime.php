d48df130c6f5d7dfb2f1c35e5438ed570fff76a0
Kashyap Sanjaybhai Pathak <77912420+kashyappathak@users.noreply.github.com>
2023-05-16 17:12:42 +0530

    Add files via upload

---
+++ dateandtime.php
@@ -0,0 +1,10 @@
+<?php
+echo "Today is " . date("Y/m/d") . "<br>";
+echo "Today is " . date("Y.m.d") . "<br>";
+echo "Today is " . date("Y-m-d") . "<br>";
+echo "Today is " . date("l");
+?>
+&copy; 2010-<?php echo date("Y");?><br>
+<?php
+echo "The time is " . date("h:i:sa");
+?>
