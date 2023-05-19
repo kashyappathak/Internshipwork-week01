f75f2498511b303cc53a2368b33d52a31cfc0251
Kashyap Sanjaybhai Pathak <77912420+kashyappathak@users.noreply.github.com>
2023-05-19 18:05:30 +0530

    export.php

--- php-crud/export.php
+++ php-crud/export.php
@@ -1,12 +1,12 @@
 <?php
-// Include the PhpSpreadsheet classes
+// Include the necessary libraries
 require 'vendor/autoload.php';
 
 use PhpOffice\PhpSpreadsheet\Spreadsheet;
-use PhpOffice\PhpSpreadsheet\Writer\Xlsx;
+use PhpOffice\PhpSpreadsheet\Writer\Csv;
 
 // Get the form data
-$product = $_POST['product'];
+$product = $_POST['id'];
 $type = $_POST['type'];
 $description = $_POST['description'];
 
@@ -22,24 +22,24 @@ include("connect-2.php"); // Include your database connection file
 $sql = "INSERT INTO category (product, type, description, image) VALUES ('$product', '$type', '$description', '$targetFilePath')";
 mysqli_query($conn, $sql);
 
+// Fetch data from the database
+$sql = "SELECT * FROM category";
+$result = mysqli_query($conn, $sql);
+
 // Create a new spreadsheet object
 $spreadsheet = new Spreadsheet();
 $sheet = $spreadsheet->getActiveSheet();
 
 // Set the column headers
-$sheet->setCellValue('A1', 'Product');
+$sheet->setCellValue('A1', 'id');
 $sheet->setCellValue('B1', 'Type');
 $sheet->setCellValue('C1', 'Description');
 $sheet->setCellValue('D1', 'Image');
 
-// Fetch data from the database
-$sql = "SELECT * FROM category";
-$result = mysqli_query($conn, $sql);
-
 $rowIndex = 2; // Start from row 2 to leave space for column headers
 
 while ($row = mysqli_fetch_assoc($result)) {
-    $sheet->setCellValue('A' . $rowIndex, $row['product']);
+    $sheet->setCellValue('A' . $rowIndex, $row['id']);
     $sheet->setCellValue('B' . $rowIndex, $row['type']);
     $sheet->setCellValue('C' . $rowIndex, $row['description']);
     $sheet->setCellValue('D' . $rowIndex, $row['image']);
@@ -47,20 +47,26 @@ while ($row = mysqli_fetch_assoc($result)) {
 }
 
 // Set the file name and extension
-$fileName = 'database_data.xlsx';
+$fileName = 'database_data.csv';
 
-// Create a writer object and save the spreadsheet as an Excel file
-$writer = new Xlsx($spreadsheet);
+// Create a writer object and save the spreadsheet as a CSV file
+$writer = new Csv($spreadsheet);
+$writer->setDelimiter(',');
+$writer->setEnclosure('"');
+$writer->setLineEnding("\r\n");
+$writer->setSheetIndex(0);
 $writer->save($fileName);
 
 // Download the file
-header('Content-Type: application/vnd.openxmlformats-officedocument.spreadsheetml.sheet');
+header('Content-Type: application/octet-stream');
 header('Content-Disposition: attachment; filename="' . $fileName . '"');
-header('Cache-Control: max-age=0');
+header('Content-Length: ' . filesize($fileName));
+readfile($fileName);
 
-$writer->save('php://output');
+// Delete the file after download
+unlink($fileName);
 
 // Redirect back to the create page
-header('Location: index-3.php');
+// header('Location: index-3.php');
 exit();
 ?>
